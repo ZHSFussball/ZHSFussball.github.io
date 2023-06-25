6ab1d3baec268736eb81589ffd426dd944cb14dd
JosefMoosholzer <jamoosholzer@hotmail.de>
2023-06-26 01:28:24 +0200

    Big commit

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ const soloLink = document.querySelector('.solo-link');
 const btnPopup = document.querySelector('.btnRegister-popup');
 const iconClose = document.querySelector('.icon-close');
 
-soloLink.addEventListener('click', ()=> {
+/*soloLink.addEventListener('click', ()=> {
     wrapper.classList.add('active');
 });
 
 teamLink.addEventListener('click', ()=> {
     wrapper.classList.remove('active');
-});
+});*/
 
 btnPopup.addEventListener('click', ()=> {
     wrapper.classList.add('active-popup');
